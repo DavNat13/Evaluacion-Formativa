979ae4302d3f359dc7e47f8a1881e6a571a52d7c
RETR0NACH <ichaconaedo@gmail.com>
2024-06-17 21:51:13 -0400

    comentarios generales

--- funciones.py
+++ funciones.py
@@ -2,19 +2,19 @@
 trabajadores = []
 cargos = ["CEO", "Desarrollador", "Analista de datos"]
 
-def registrar_trabajador():
+def registrar_trabajador():    #Ingresamos los datos del trabajor
     nombre = input("Ingrese el nombre del trabajador: ")
     apellido = input("Ingrese el apellido del trabajador: ")
     cargo = input("Ingrese el cargo del trabajador: ")
     try:
-        sueldo_bruto = float(input("Ingrese el sueldo bruto del trabajador: "))
+        sueldo_bruto = float(input("Ingrese el sueldo bruto del trabajador: ")) #Ingreso de sueldo del trabajador
     except:
         print("Dato no válido...")
     # Cálculo de descuentos y sueldo líquido
     desc_salud = sueldo_bruto * 0.07
     desc_afp = sueldo_bruto * 0.12
     sueldo_liquido = sueldo_bruto - desc_salud - desc_afp
-
+#Diccionario 
     trabajador = {
         "Nombre": nombre,
         "Apellido": apellido,
@@ -24,15 +24,15 @@ def registrar_trabajador():
         "Desc. AFP": desc_afp,
         "Líquido a pagar": sueldo_liquido
     }
-    with open("trabajadores.txt", "a", encoding='utf-8') as archivo:
-        archivo.write("Nombre\t Apellido\tCargo \tSueldo Bruto \tDesc. Salud \tDesc. AFP \tLíquido a pagar\n")
-        archivo.write(f"{nombre}\t{apellido} \t{cargo}\t {sueldo_bruto} \t {desc_salud}\t{desc_afp} \t{sueldo_liquido}\n")
+    with open("trabajadores.txt", "a", encoding='utf-8') as archivo: #Creamos un archivo txt
+        archivo.write("Nombre\t Apellido\tCargo \tSueldo Bruto \tDesc. Salud \tDesc. AFP \tLíquido a pagar\n") #Fila
+        archivo.write(f"{nombre}\t{apellido} \t{cargo}\t {sueldo_bruto} \t {desc_salud}\t{desc_afp} \t{sueldo_liquido}\n") #Fila con los resultados 
         archivo.write("\n")
-    trabajadores.append(trabajador)
+    trabajadores.append(trabajador) #Se agrega el trabajador a la lista 
     print("Trabajador registrado exitosamente.\n")  
 def mostrarr():
     print(trabajadores)
-def salir():
+def salir(): #Saliendo del programa 
     print("Saliendo del programa...")
     exit()
 
